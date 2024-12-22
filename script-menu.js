cf7f87f0b77d6b2b91bfeac3d6d14223cb4c0a31
Noah <pancakeboy55@gmail.com>
2024-12-22 14:07:06 -0800

    Added menu for mobile

---
+++ script-menu.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    const menu = document.getElementById("menu");
+    menu.style.display = "flex";
+}
+
+function closeMenu() {
+    const menu = document.getElementById("menu");
+    menu.style.display = "none";
+}
